a32b82e09c77a977d7ba5c9292642fc1876903ce
Anuja Sudarshan <anuja.sudarshan@gmail.com>
2019-08-02 15:30:48 -0700

    project init

---
+++ codealongapi.js
@@ -0,0 +1,32 @@
+var ourLoc;
+var view;
+var map;
+
+function init() {
+  ourLoc = ol.proj.fromLonLat([-122.347992, 47.648029]);
+
+  view = new ol.View({
+          center: ourLoc,
+          zoom: 4
+        })
+
+    map = new ol.Map({
+          target: 'map',
+          layers: [
+           new ol.layer.Tile({
+             source: new ol.source.OSM()
+           })
+         ],
+         view
+       });
+
+}
+
+function panHome() {
+  view.animate({
+    center:ourLoc,
+    duration: 2000
+  });
+}
+
+window.onload = init;
